d17fbcae2c5fad35d31730e0f79a27e87149d787
Zack Johnson <zaj8224@gmail.com>
2026-04-25 15:08:40 -0400

    Updated hint logic

--- app.py
+++ app.py
@@ -7,4 +7,22 @@ def display_intro():
     print("You will get hints after each incorrect guess.")
     print()
 
-display_intro()
+def find_player(guess):
+    for player in players_list:
+        if player["name"].lower() == guess.lower():
+            return player
+    return None
+
+def hints(guess_player, mystery_player):
+    print("\nFeedback:")
+
+    categories = ["team", "league", "position", "bats", "throws"]
+
+    for category in categories:
+        if guess_player[category] == mystery_player[category]:
+            print(f"{category.title()}: Correct")
+        else:
+            print(f"{category.title()}: {guess_player[category]} is incorrect")
+
+    print()
+    
